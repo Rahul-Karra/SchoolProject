58f8e1d5ae774e43490950c258c003bcd39c4e39
NimblixTechnologies <tejaswininimblixtechnologies@gmail.com>
2025-11-24 11:00:23 +0530

    StudentRegistration
    StudentRegistration

--- SchoolPEPProject/src/main/java/com/nimblix/SchoolPEPProject/Controller/StudentController.java
+++ SchoolPEPProject/src/main/java/com/nimblix/SchoolPEPProject/Controller/StudentController.java
@@ -1,4 +1,27 @@
 package com.nimblix.SchoolPEPProject.Controller;
 
+import com.nimblix.SchoolPEPProject.Request.StudentRegistrationRequest;
+import com.nimblix.SchoolPEPProject.Service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/student")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    /*
+    In this API we are registering the student. It will help to onboard the student
+     */
+    public ResponseEntity<?> studentRegistration(@RequestBody StudentRegistrationRequest studentRegistrationRequest){
+        return  studentService.registerStudent(studentRegistrationRequest);
+    }
+
 }
